eb8ae6437f8dcaeffe681818150744d58523c338
wangmeikai <915672375@qq.com>
2020-12-31 16:37:42 +0800

    添加数据库

--- src/main/java/com/wmk/onlinetelebook/OnlinetelebookApplication.java
+++ src/main/java/com/wmk/onlinetelebook/OnlinetelebookApplication.java
@@ -1,11 +1,13 @@
 package com.wmk.onlinetelebook;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan("com.wmk.onlinetelebook.filter")
+@MapperScan("com.wmk.onlinetelebook.mapper")
 public class OnlinetelebookApplication {
 
     public static void main(String[] args) {
